9cd6d424936640b2e73e6f2401246a75ae65d371
janat <janat@agusPC>
2024-02-19 12:07:06 -0300

    cambios asignaturas 19/2 12:07

--- Taller 2 UDE/src/CapaLogica/Asignaturas/Asignaturas.java
+++ Taller 2 UDE/src/CapaLogica/Asignaturas/Asignaturas.java
@@ -14,19 +14,33 @@ public class Asignaturas {
 	}
 
     public boolean member(String cod) {
-    	
+    	/*
+    	boolean esta;
+    	for (Asignatura asi : arre) {
+            if (asi.getCodigo().equals(cod)) {
+                esta=true;
+            }
+        }
+        return esta;
+        */
     }
     
     public Asignatura find(String cod) {
-    	
+    	/*
+    	 for (Asignatura asi : arre) {
+            if (asi.getCodigo().equals(cod)) {
+                return asignatura;
+            }
+        }
+        */
     }
     
     public void insBack(Asignatura asi) {
-    	
+    	//arre.add(asi);
     }
     
     public boolean esVacio() {
-    	
+    	//return arre.isEmpty();
     }
     
     public voAsignatura [] listaAsignaturas() {
